05391b6317e15958a7e467822df159a4074bad7d
Fall31 <lazomorenowilsonlazo@gmail.com>
2026-09-25 14:54:48 -0400

    wil:Busqueda de ICategoryDao

--- src/main/java/com/company/inventory/dao/ICategoryDao.java
+++ src/main/java/com/company/inventory/dao/ICategoryDao.java
@@ -1,9 +1,17 @@
 package com.company.inventory.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.company.inventory.model.Category;
 
-public interface ICategoryDao extends CrudRepository<Category, Long>{
+public interface ICategoryDao extends CrudRepository<Category, Long> {
+
+    @Query("select c from Category c where c.name like %?1%")
+    List<Category> findByNameLike(String name);
+
+    List<Category> findByNameContainingIgnoreCase(String name);
 
 }
