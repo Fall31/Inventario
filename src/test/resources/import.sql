c184bd6d6a1234b4fd05ecfa3c822182529567fc
Fall31 <lazomorenowilsonlazo@gmail.com>
2026-09-25 15:49:17 -0400

    wil:Añadir test para IProductDao y valores para product en import.sql¨¨Â

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,3 +1,8 @@
 INSERT INTO category (name, description) VALUES ('Electronics', 'Devices and gadgets');
 INSERT INTO category (name, description) VALUES ('Books', 'Printed and digital books');
 INSERT INTO category (name, description) VALUES ('Clothing', 'Apparel and accessories');
+
+INSERT INTO product (name, price, account, category_id) VALUES ('Laptop', 1000, 5, 1);
+INSERT INTO product (name, price, account, category_id) VALUES ('Smartphone', 700, 10, 1);
+INSERT INTO product (name, price, account, category_id) VALUES ('Java Programming', 50, 20, 2);
+INSERT INTO product (name, price, account, category_id) VALUES ('T-Shirt', 15, 100, 3);
